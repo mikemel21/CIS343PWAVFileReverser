ac38aaba091937a4870891ec8d5a58a059a29c61
mikemel21 <mikemelei905@gmail.com>
2023-09-13 13:25:20 -0400

    Began reading bytes 4-7 to see if they = size-8

--- main.c
+++ main.c
@@ -4,20 +4,23 @@
 
 //* returns 1 if the first 4 bytes of WAV file are "RIFF" and 0 if they are not
 int isRIFF (char* fileContents);
-//* returns 1 if bytes 8-11 are "WAVE" and 0 if they are not
-int isWAVE (char* fileContacts);
 
 //! to compile using clang, use following command: clang -o reversed *.c
 
 int main(int argc, char** argv) { 
     size_t sizeOfFile; // size of WAV file
     char* fileContents = read_file(argv[1], &sizeOfFile); // stores the WAV file contents
+    printf("actual file size: %d\n", sizeOfFile);
 
-    int RIFF = isRIFF(fileContents); // 1 if first 4 bytes are "RIFF", 0 if not
-    if (isRIFF(fileContents) == 0) {
+    if (isRIFF(fileContents) == 1) {
         printf("Unable to process file.\n");
         exit(1);
     }
+
+    // read bytes 4-7 and see if they equal the sizeOfFile - 8;
+    // ! you need to read the bytes backwards (7, 6, 5, then 4)
+    
+
     free (fileContents);
     return 0;
 }
@@ -26,8 +29,8 @@ int isRIFF (char* fileContents) {
     char RIFF[4] = "RIFF";
     for (int i = 0; i < 4; i++) {
         if (fileContents[i] != RIFF[i]) {
-            return 0;
+            return 1;
         }
     }
-    return 1;
+    return 0;
 }
