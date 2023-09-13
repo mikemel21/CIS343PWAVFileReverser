f5a9af3ce225414a7e2fe9e127fb898447f729b8
mikemel21 <mikemelei905@gmail.com>
2023-09-12 20:15:26 -0400

    Added a check to ensure first 4 bytes are RIFF

--- main.c
+++ main.c
@@ -7,14 +7,17 @@ int isRIFF (char* fileContents);
 //* returns 1 if bytes 8-11 are "WAVE" and 0 if they are not
 int isWAVE (char* fileContacts);
 
-//! to compile using clang, use following command: clang -o a *.c
+//! to compile using clang, use following command: clang -o reversed *.c
 
 int main(int argc, char** argv) { 
     size_t sizeOfFile; // size of WAV file
     char* fileContents = read_file(argv[1], &sizeOfFile); // stores the WAV file contents
 
     int RIFF = isRIFF(fileContents); // 1 if first 4 bytes are "RIFF", 0 if not
-
+    if (isRIFF(fileContents) == 0) {
+        printf("Unable to process file.\n");
+        exit(1);
+    }
     free (fileContents);
     return 0;
 }
