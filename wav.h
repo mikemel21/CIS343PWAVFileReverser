ab377c0fd3f7e5d8dbf9ea50c1e870791b900cad
mikemel21 <mikemelei905@gmail.com>
2023-09-16 17:35:38 -0400

    Added more functions to wav.c and wav.h

--- wav.h
+++ wav.h
@@ -2,18 +2,25 @@
 #define		__H_WAV__
 #include <stdio.h>
 
-//* returns 0 if the first 4 bytes of WAV file are "RIFF" and 1 if they are not
-int isRIFF (char* fileContents);
+// WAV header struct
+typedef struct wh {
+    char header[44];
+} wave_header;
 
-//* returns 0 if bytes 4-7 equal the sizeOfFile-8 and 2 if not
-int bits4to7 (char* fileContents, size_t size);
+//WAV file struct
+typedef struct wf {
+    wave_header header;
+    size_t fileSize;
+    char* dataPointer;
+} wave_file;
 
-//* returns 0 if bytes 8-11 = WAVE and 1 if not
-int isWAVE (char* fileContents);
+// takes in a file and creates a header struct
+wave_header createHeader (char* fileContents);
 
-//* returns 0 if file has two channels and 1 if it does not
-int has2Channels (char* fileContents);
+// loads wave file
+wave_file* loadWAV(const char* path);
 
-//* returns 0 if file has format type 1 and 1 if it does not
-int formatType (char* fileContents);
+// creates a byte array of the file
+int prepByteArray (struct wf);
+
 #endif
