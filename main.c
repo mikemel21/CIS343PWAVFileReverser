e7167e962d372914f35f07f201f3c630135aca19
mikemel21 <mikemelei905@gmail.com>
2023-09-18 21:16:29 -0400

    completed reverser

--- main.c
+++ main.c
@@ -12,29 +12,31 @@
 int main(int argc, char** argv) { 
     wave_file* waveFile = loadWAV(argv[1]);
     wave_file reversedFile;
+    // allocate memory to data pointer of reverse file. ensures it has enough memory to store the full data section of wav
+    reversedFile.dataPointer = malloc(waveFile->fileSize);
+
     // validates file can be reversed
     checkHeader(waveFile);
     
-    // just a direct copy
     reversedFile.fileSize = waveFile->fileSize;
-    reversedFile.header = waveFile->header;
-    //reversedFile.dataPointer = waveFile->dataPointer;
-
-    //memcpy(reversedFile.dataPointer, waveFile->dataPointer, waveFile->fileSize);
+    memcpy(reversedFile.header.header, waveFile->header.header, 44);
 
     int count = 0;
-    for (int i = 0; i < waveFile->fileSize ; i++) {
-        reversedFile.dataPointer[count++] = *waveFile->dataPointer+(waveFile->fileSize-i);
+    for (int i = waveFile->fileSize-4; i >= 0; i -= 4) {
+        memcpy(&reversedFile.dataPointer[count], &waveFile->dataPointer[i], 4);
+        count += 4;
     }
 
+    // output results
     printf("Input File Name: %s\n", argv[1]);
     printf("File Size: %zu\n", waveFile->fileSize);
     printf("Sample Rate: %u\n", getSampleRate(waveFile));
     printf("# Channels: %d\n", waveFile->header.header[22]); // get # channels from byte 22
     printf("Output File Name: %s\n", "");
 
-    //SWITCH TO THIS: int error = prepByteArray(*reversedFile, argv[2]);
-    int error = prepByteArray(reversedFile, "reversed.wav");
+    int error = prepByteArray(reversedFile, argv[2]);
+
     free (waveFile);
+    free (reversedFile.dataPointer);
     return 0;
 }
